85d847140c9b41db675770d19821d5d6330be8fb
wilferdperante <34180512+wilferdperante@users.noreply.github.com>
2025-05-03 10:04:26 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,11 +11,8 @@ from nltk.stem.porter import PorterStemmer  #text processing
 import pandas as pd
 ps=PorterStemmer()   
 from xgboost import XGBClassifier
-<<<<<<< HEAD
-=======
 nltk.download('punkt')
 nltk.download('stopwords')
->>>>>>> 3d47b8555834dff27e3b68b93ead18c2f8d13b7f
 #function to convert SMS text to numerical form ,SMS text  we will receive on our deployed app to predict.
 def transform_text(text):
         text=text.lower()
@@ -46,7 +43,7 @@ st.title("SMS Spam classifier")
  
  
  
-text input where user will enter the SMS text to predict (As shown above)
+#text input where user will enter the SMS text to predict (As shown above)
 input_sms= st.text_area("Enter the message")
  
 #predict button , when clicked will execute the process
@@ -54,13 +51,13 @@ if st.button('Predict'):
  
 #1.preprocess- converting the input_sms received by user on app  
  
-transform_sms=transform_text(input_sms)
+    transform_sms=transform_text(input_sms)
     print(type(transform_sms))
     transform_sms=np.array(transform_sms) #converting the list of string format to array of string format
  
 #2.vectorize - converting the received text SMS into numeric for model understanding
  
-vector_input=tfidf.transform(transform_sms.astype('str')).toarray()
+    vector_input=tfidf.transform(transform_sms.astype('str')).toarray()
     print(type(vector_input))
     print(vector_input)
     vector_input =    pd.DataFrame(vector_input,columns=tfidf.get_feature_names_out())
@@ -71,8 +68,8 @@ prediction= model.predict(vector_input)[0]
  
 #4.display- the result on app itself , if prediction result is 1 then ui(button) will display  Spam else Not Spam
     
-    if prediction==1:
-        st.header("Spam")
-    else:
-        st.header("Not Spam")
+if prediction==1:
+    st.header("Spam")
+else:
+    st.header("Not Spam")
 
