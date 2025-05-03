e1d4f43a8904a15363e7e8b6bae791f8f7ff6e00
wilferdperante <34180512+wilferdperante@users.noreply.github.com>
2025-05-03 10:11:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,76 +1,71 @@
-import streamlit as st  #used for streamlit api reference
-# below all libraries were part of SMS Spam classifier model building and hence add them again.
-import pickle     #to load the saved pickle files
+import streamlit as st
+import pickle
 import pandas as pd
 import numpy as np
 import string
-import nltk      #natural language tool kit used for text processing
-from nltk.corpus import stopwords  #text processing
-import string
-from nltk.stem.porter import PorterStemmer  #text processing
-import pandas as pd
-ps=PorterStemmer()   
+import nltk
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
 from xgboost import XGBClassifier
+
+# Download necessary NLTK data
 nltk.download('punkt')
 nltk.download('stopwords')
-#function to convert SMS text to numerical form ,SMS text  we will receive on our deployed app to predict.
+
+# Initialize stemmer
+ps = PorterStemmer()
+
+# Function to preprocess the text
 def transform_text(text):
-        text=text.lower()
-        y=[]
-        #tokenization
-        text=nltk.word_tokenize(text)
-        for i in text:
-            if i.isalnum():
-                y.append(i)
-        text=y[:]
-        y.clear()
-        #removing stopwords and punctuations
-        for i in text:
-            if i not in stopwords.words('english') and i not in string.punctuation:
-                y.append(i)
-        text=y[:]
-        y.clear()
-    
-        #stemming applied on text
-        for i in text:
-            y.append(ps.stem(i))
-        return y
-#loading  both the models from respective directory
-tfidf=pickle.load(open('SMS_Spam_Classifier/vectorizer.pkl','rb'))
-model=pickle.load(open('SMS_Spam_Classifier/mnb_spam_detector.pkl','rb'))
-#streamlit app title
-st.title("SMS Spam classifier")        
- 
- 
- 
-#text input where user will enter the SMS text to predict (As shown above)
-input_sms= st.text_area("Enter the message")
- 
-#predict button , when clicked will execute the process
+    text = text.lower()
+    y = []
+    text = nltk.word_tokenize(text)
+
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
+
+# Load the models
+tfidf = pickle.load(open('SMS_Spam_Classifier/vectorizer.pkl','rb'))
+model = pickle.load(open('SMS_Spam_Classifier/mnb_spam_detector.pkl','rb'))
+
+# Streamlit app UI
+st.title("📩 SMS Spam Classifier")
+
+# Text input for user
+input_sms = st.text_area("Enter the message")
+
+# Prediction block
 if st.button("Predict"):
     if input_sms:
-        # Convert text to vector form using vectorizer
-        vector_input = vectorizer.transform([input_sms])
+        # Preprocess text
+        transformed_sms = transform_text(input_sms)
 
-        # Predict using the model
+        # Convert to vector
+        vector_input = tfidf.transform([transformed_sms])
+
+        # Predict
         prediction = model.predict(vector_input)[0]
 
         # Display result
         if prediction == 1:
-            st.success("This is a Spam message.")
+            st.success("🚨 This is a **Spam** message.")
         else:
-            st.info("This is a Ham (Not Spam) message.")
+            st.info("✅ This is a **Ham (Not Spam)** message.")
     else:
         st.warning("Please enter a message first!")
- 
-#3.predict - passing the converted text to model to predict if it is spam or ham
- 
-prediction= model.predict(vector_input)[0]
- 
-#4.display- the result on app itself , if prediction result is 1 then ui(button) will display  Spam else Not Spam
-    
-if prediction==1:
-    st.header("Spam")
-else:
-    st.header("Not Spam")
-
